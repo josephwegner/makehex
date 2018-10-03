2759542085e131760a6c3ca0e317ef6e5c387821
Joe Wegner <joe@wegnerdesign.com>
2018-10-03 16:37:46 -0500

    more logging

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,15 @@
 require 'connection_pool'
 
 Sidekiq.configure_client do |config|
+  puts 'Opening sidekiq client redis connection'
+  config.redis = ConnectionPool.new(size: ENV['SIDEKIQ_REDIS_POOL'] || 5, timeout: 5) do
+    Redis.new url: ENV['REDIS_URL']
+  end
+end
+
+
+Sidekiq.configure_server do |config|
+  puts 'Opening sidekiq server redis connection'
   config.redis = ConnectionPool.new(size: ENV['SIDEKIQ_REDIS_POOL'] || 5, timeout: 5) do
     Redis.new url: ENV['REDIS_URL']
   end
