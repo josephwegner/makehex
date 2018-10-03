35719cf962d565b1e280dedd3fa85236efc9d192
Joe Wegner <joe@wegnerdesign.com>
2018-10-03 16:33:28 -0500

    try a new sidekiq connection pool strategy

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
-redis_conn = proc {
-  Redis.new # do anything you want here
-}
+require 'connection_pool'
 
 Sidekiq.configure_client do |config|
-  config.redis = ConnectionPool.new(size: ENV['SIDEKIQ_REDIS_POOL'] || 5, &redis_conn)
+  config.redis = ConnectionPool.new(size: ENV['SIDEKIQ_REDIS_POOL'] || 5, timeout: 5) do
+    Redis.new url: ENV['REDIS_URL']
+  end
 end
