a4cdbff59dbceeb228daa6249ac038a92f11e43f
Joe Wegner <joe@wegnerdesign.com>
2019-06-18 11:25:59 -0500

    better links for logged in folks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def after_sign_in_path_for(resource)
+    maps_path
+  end
+
   def not_found
     raise ActionController::RoutingError.new('Not Found')
   end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,11 @@
 class HomeController < ApplicationController
   layout 'marketing', :except => [:pricing]
   def index
+    if user_signed_in?
+      @cta_path = maps_path
+    else
+      @cta_path = new_registration_path(User)
+    end
   end
 
   def pricing
