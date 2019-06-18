747adaa2499da3a7b281d230f2763311b6273f0f
Joe Wegner <joe@wegnerdesign.com>
2019-06-17 22:28:40 -0500

    marketing site

--- app/controllers/layouts_controller.rb
+++ app/controllers/layouts_controller.rb
@@ -1,4 +1,5 @@
 class LayoutsController < ApplicationController
+  before_action :authenticate_user!
 
   def create
     @map = Map.find(params[:map])
--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -1,4 +1,27 @@
 class MapsController < ApplicationController
+  before_action :authenticate_user!, :except => [:show]
+
+  def index
+    @map_colors = {}
+    current_user.maps.each do |map|
+      colors = {}
+      map.layouts.each do |layout|
+        layout.grid.each_value do |q|
+          q.each_value do |tile|
+            if tile.has_key?('color')
+              colors[tile['color']] ||= 0
+              colors[tile['color']] += 1
+            end
+          end #q.each
+        end #grid.each
+      end #layouts.each
+
+      @map_colors[map.id] = colors.max_by(5) { |k,v| v }.map { |obj|
+        # Wee bit of XSS protection
+        obj[0].gsub(/[^A-Za-z0-9#]/, '')[0...7]
+      }
+    end #maps.each
+  end
 
   def show
     # If the id is numeric, this is a direct map load. Otherwise it is an access code
@@ -45,6 +68,7 @@ class MapsController < ApplicationController
   private
 
   def show_map(params)
+    authenticate_user!
     @map = Map.find_by_id(params[:id])
 
     if @map.user == current_user
