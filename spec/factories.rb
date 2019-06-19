f8a4106f887e527f07c24d74e474a34de8838e99
Joe Wegner <joe@wegnerdesign.com>
2019-06-19 06:55:22 -0500

    a view test

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,26 @@
 FactoryBot.define do
   factory :user do
-    email { "joe@gmail.com" }
+    sequence :email do |n|
+      "person#{n}@example.com"
+    end
     password { "blahblah" }
+  end
+
+  factory :map do
+    sequence :access_code do |n|
+      "aa#{n}bb"
+    end
+    user
+  end
+
+  factory :player do
+    sequence :code do |n|
+      "aa#{n}bb"
+    end
+    sequence :token_label do |n|
+      "#{n}#{n}"
+    end
+    token_color { "CCCCCC" }
+    map
   end
 end
